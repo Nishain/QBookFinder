700aaa403e8a11497877285d38172dd2deb3ff35
Nishain <nishain.atomic@gmail.com>
2021-12-29 16:24:34 +0530

    bookstore create function

--- lib/BookStoreScreen.dart
+++ lib/BookStoreScreen.dart
@@ -7,6 +7,7 @@ import 'package:q_book_finder/LocationUpdateCallback.dart';
 import 'package:q_book_finder/compoenents/MaterialTextField.dart';
 import 'package:q_book_finder/compoenents/RedButton.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
 class BookStoreScreen extends StatefulWidget with LocationUpdateCallback {
@@ -17,71 +18,101 @@ class BookStoreScreen extends StatefulWidget with LocationUpdateCallback {
   State<BookStoreScreen> createState() => BookStoreState();
 }
 
+enum fieldNames { shopName, shopOwner }
+
 class BookStoreState extends State<BookStoreScreen> {
   String? location;
   GoogleMapController? controller;
-
+  String? shopID;
   late String endpoint;
+  final String SHOP_ID_KEY = "shopID";
   @override
-  void initState(){
-    dotenv.load(fileName : '.env')
-    .then((value) => {
-      endpoint = "${dotenv.env['localendpoint']!}:${dotenv.env['port']!}",
-      log('LOCAL ENDPOINT $endpoint'),
-      http.get(Uri.parse('http://$endpoint/bookStore/61c07eccb9dba854e2c4e24a')).then((result){
-        dynamic data = jsonDecode(result.body);        
-        setState(() {
-          name = data['name'];
-          ownerName = data['shopOwner'];
-        });
-        
-      })
-    });
-    
+  void initState() {
     super.initState();
+    dotenv.load(fileName: '.env').then((value) => {
+          endpoint = "http://${dotenv.env['localendpoint']!}:${dotenv.env['port']!}",
+          log('LOCAL ENDPOINT $endpoint'),
+          SharedPreferences.getInstance().then((sharedPreference) {
+            shopID = sharedPreference.getString(SHOP_ID_KEY);
+            if (shopID != null) {
+              http.get(Uri.parse('$endpoint/bookStore/$shopID'))
+                  .then((result) {
+                dynamic data = jsonDecode(result.body);
+                fieldInputs[fieldNames.shopName]!.controller.text =
+                    data['name'] as String;
+                fieldInputs[fieldNames.shopOwner]!.controller.text =
+                    data['shopOwner'] as String;
+              });
+            }
+          })
+        });
   }
-  showDeleteConfirmationMessage(){
-    showDialog(context: context, builder: (_)=> AlertDialog(
-      title: const Text("Are you sure?"),
-      content: const Text("Do you want to delete your shop? this action is unreversible. Continue?"),
-      actions: [
-        TextButton(onPressed: ()=>Navigator.of(context).pop(), child: const Text("Cancel",style: TextStyle(color: Colors.red))),
-        ElevatedButton(onPressed: ()=>{},child: const Text("Delete",style: TextStyle(color: Colors.white)),style: ElevatedButton.styleFrom(primary: Colors.red))
-      ],
-    ));
+
+  showDeleteConfirmationMessage() {
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: const Text("Are you sure?"),
+              content: const Text(
+                  "Do you want to delete your shop? this action is unreversible. Continue?"),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text("Cancel",
+                        style: TextStyle(color: Colors.red))),
+                ElevatedButton(
+                    onPressed: () => {},
+                    child: const Text("Delete",
+                        style: TextStyle(color: Colors.white)),
+                    style: ElevatedButton.styleFrom(primary: Colors.red))
+              ],
+            ));
   }
-  resolveAction(action){
-      setState(() {
-        updateMode = (action == 'update');
-      });
-    
-  }
-  String? name,ownerName,isbn;
-  updateMapToCurrentLocation({Function? notifier}) async{
-    if(notifier!=null){
+
+  resolveAction(action)async {
+    setState(() {
+      updateMode = (action == 'update');
+    });
+    if (action == 'create') {
+      Map<fieldNames, String> data = {};
+      data.addAll(fieldInputs
+          .map((key, value) => MapEntry(key, value.controller.text)));
+      String postBody = jsonEncode({'name':fieldInputs[fieldNames.shopName]!.controller.text,'shopOwner':fieldInputs[fieldNames.shopOwner]!.controller.text});
+      http.Response response = await http.post(
+        Uri.parse("$endpoint/BookStore/"),
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        body:postBody
+      );    
+      SharedPreferences.getInstance().then((sharedPreference)=>sharedPreference.setString(SHOP_ID_KEY, jsonDecode(response.body)['_id']));
     }
+  }
+
+  String? name, ownerName, isbn;
+  updateMapToCurrentLocation({Function? notifier}) async {
+    if (notifier != null) {}
     dynamic result = (await widget.getLocation(notifier));
-    if(result == 'locked'){
+    if (result == 'locked') {
       return;
     }
     LatLng? position = result as LatLng?;
     if (position != null) {
-      controller!.animateCamera(
-          CameraUpdate.newCameraPosition(CameraPosition(
+      controller!.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
         target: position,
         zoom: 17.0,
       )));
       setState(() {
-        
-        markers.add(Marker(
-            markerId: const MarkerId('Shop'),
-            position: position));
+        markers
+            .add(Marker(markerId: const MarkerId('Shop'), position: position));
       });
     }
   }
- 
+
   Set<Marker> markers = {};
   bool updateMode = false;
+  Map<fieldNames, MaterialTextField> fieldInputs = {
+    fieldNames.shopName: MaterialTextField("Name of the shop"),
+    fieldNames.shopOwner: MaterialTextField("Shop Owner"),
+  };
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -95,23 +126,38 @@ class BookStoreState extends State<BookStoreScreen> {
                     "Create a Book Store",
                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   )),
-              MaterialTextField("Name of the shop",initialValue:name),
-              MaterialTextField("Shop Owner",initialValue:ownerName),
-              if(updateMode)
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                Expanded(child: MaterialTextField("ISBN"),flex: 2),
-                Expanded(child:Padding(padding: const EdgeInsetsDirectional.only(start: 7,end: 7),child: ElevatedButton(onPressed: ()=>{},child: const Text("Add Book",textAlign: TextAlign.center,))) 
-                )
-                
-              ]),
+              fieldInputs[fieldNames.shopName] as Widget,
+              fieldInputs[fieldNames.shopOwner] as Widget,
+              if (updateMode)
+                Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                  Expanded(child: MaterialTextField("ISBN"), flex: 2),
+                  Expanded(
+                      child: Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              start: 7, end: 7),
+                          child: ElevatedButton(
+                              onPressed: () => {},
+                              child: const Text(
+                                "Add Book",
+                                textAlign: TextAlign.center,
+                              ))))
+                ]),
               ButtonBar(
                 alignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(onPressed: ()=>resolveAction('create'), child: const Text("Create",textAlign: TextAlign.center,)),
-                  ElevatedButton(onPressed: () => {resolveAction('update')}, child: const Text("Update",textAlign: TextAlign.center,)),
-                  RedButton("Delete Shop",showDeleteConfirmationMessage)
+                  ElevatedButton(
+                      onPressed: () => resolveAction('create'),
+                      child: const Text(
+                        "Create",
+                        textAlign: TextAlign.center,
+                      )),
+                  ElevatedButton(
+                      onPressed: () => {resolveAction('update')},
+                      child: const Text(
+                        "Update",
+                        textAlign: TextAlign.center,
+                      )),
+                  RedButton("Delete Shop", showDeleteConfirmationMessage)
                 ],
               ),
               Expanded(
@@ -121,9 +167,9 @@ class BookStoreState extends State<BookStoreScreen> {
                     initialCameraPosition: widget.initialPosition,
                     markers: markers,
                     onMapCreated: (GoogleMapController controller) async {
-                      
                       this.controller = controller;
-                      updateMapToCurrentLocation(notifier: updateMapToCurrentLocation);
+                      updateMapToCurrentLocation(
+                          notifier: updateMapToCurrentLocation);
                     },
                   ),
                   Align(
--- lib/compoenents/MaterialTextField.dart
+++ lib/compoenents/MaterialTextField.dart
@@ -4,16 +4,15 @@ import 'package:flutter/material.dart';
 
 
 class MaterialTextField extends StatelessWidget{
-  final String label;
-  final bool readOnly;
-  final TextEditingController controller =  TextEditingController();
+   final String label;
+   final bool readOnly;
+  final TextEditingController controller = TextEditingController();
   final String? initialValue;
-  MaterialTextField(this.label,{Key? key,this.readOnly = false,this.initialValue}) : super(key: key);
+  MaterialTextField(this.label,{Key? key,this.readOnly = false,this.initialValue });
+
   @override
   Widget build(BuildContext context) {
-    controller.text = initialValue ?? "";
-    return 
-    Padding(padding: const EdgeInsets.only(bottom: 10),child: TextFormField(controller: controller,readOnly: readOnly ,decoration: InputDecoration(border: const OutlineInputBorder(),labelText: label)));
+    return Padding(padding: const EdgeInsets.only(bottom: 10),child: TextFormField(controller: controller,readOnly: readOnly ,decoration: InputDecoration(border: const OutlineInputBorder(),labelText: label)));
   }
   
 }
